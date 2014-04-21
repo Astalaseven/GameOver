4aca2d360a1e9a39261dfd00946810793c106543
Gatien Bovyn <gatien.bovyn@gmail.com>
2014-04-21 16:20:30 +0200

    Remove public access

--- src/g39189/gameover/model/InitPosition.java
+++ src/g39189/gameover/model/InitPosition.java
@@ -5,36 +5,16 @@ package g39189.gameover.model;
  * 
  * @author Bovyn Gatien - 39189
  */
-public enum InitPosition
+enum InitPosition
 {
     NORD_OUEST(-1, 0), SUD_EST(5, 4), NORD_EST(0, 5), SUD_OUEST(4, -1);
 
-    private final int column;
-    private final int row;
+    final int column;
+    final int row;
 
     private InitPosition(int row, int column)
     {
         this.column = column;
         this.row = row;
-    }
-
-    /**
-     * Retourne la valeur de column
-     * 
-     * @return column colonne dans le tableau
-     */
-    int getColumn()
-    {
-        return column;
-    }
-
-    /**
-     * Retourne la valeur de row
-     * 
-     * @return row ligne dans le tableau
-     */
-    int getRow()
-    {
-        return row;
     }
 }
